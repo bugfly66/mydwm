932fb81cd0b2f8e761ab8a8f34da514f875de02b
2395386903@qq.com <2395386903@qq.com>
2024-08-19 15:25:30 +0800

    underlinetags barpadding and minor modifications

--- config.h
+++ config.h
@@ -5,10 +5,13 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 18;        /* 2 is the default spacing around the bar's font */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10; 
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "monospace:size=12","WenQuanYi Micro Hei:size=12:type=Regular:antialias=true:autohint=true","JetBrainsMono Nerd Font:pixelsize=24:type=Regular;antialias=true:autohint=true","JoyPixels:pixelsize=24:type=Regular:antialias=true:autohint=true",        };
 static const char dmenufont[]       = "monospace:size=12";
@@ -16,6 +19,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_gray5[]       = "#63e398";
 static const char col_cyan[]        = "#005577";
 static const unsigned int baralpha = 0x99;
 static const unsigned int borderalpha = OPAQUE;
@@ -23,7 +27,7 @@ static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray5, col_gray1,  col_cyan  },
 };
 
 static const unsigned int alphas[][3]      = {
@@ -47,6 +51,10 @@ static const char *const autostart[] = {
 
 /* tagging */
 static const char *tags[] = { "甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬","癸" };
+static const unsigned int ulinepad	= 0;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 4;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
 static const Rule rules[] = {
 	/* xprop(1):
